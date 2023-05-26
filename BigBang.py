ab5c3b1617956a38c9e25c7cab95915dcb1e8124
Raymond0209 <raymondjee9@gmail.com>
2023-05-26 17:17:21 +0800

    Finished the code

---
+++ BigBang.py
@@ -0,0 +1,22 @@
+import json
+
+#Initialize the result list
+result = []
+
+#Loop through the numbers 1 to 100
+for num in range(1, 101):
+    #Check if the number is divisible by 3 and 5
+    if num % 3 == 0 and num % 5 == 0:
+        result.append("BIGBANG")
+    #Check if the number is divisible by 3
+    elif num % 3 == 0:
+        result.append("BIG")
+    #Check if the number is divisible by 5
+    elif num % 5 == 0:
+        result.append("BANG")
+    else:
+        result.append(str(num))
+
+#Write the result to a file
+with open('output.json', 'w') as outfile:
+    json.dump(result, outfile)
